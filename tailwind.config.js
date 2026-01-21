a29c1c9aecb04ea8186ae82284c33c4ac976b418
UserLaBriq <anstettjulien@gmail.com>
2026-01-21 17:21:23 +0100

    Ajout hero avec fond Paris et design+

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#1E40AF',
+        'primary': '#8B0000',
         'text-dark': '#1F2937',
         'text-medium': '#6B7280',
         'border-light': '#E5E7EB',
       },
       fontFamily: {
+        'inter': ['"Inter"', 'sans-serif'],
         'condensed': ['"Roboto Condensed"', 'sans-serif'],
-        'sans': ['Open Sans', 'sans-serif'],
+        'sans': ['"Open Sans"', 'sans-serif'],
       },
       maxWidth: {
         'container': '1140px',
